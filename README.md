384a27defc1cd0632632cd8faaa77c2b02a12425
Mario Becker <mario.becker@gmx.de>
2017-11-15 10:39:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+iCloudPhotoViewer is a small python app that shows your iCloud photos on a RaspberryPi with TFT screen.
+It uses the framebuffer devices /dev/fb0 as graphical output, so no X is required, thus it saves resources on your Pi.
+Tested with RetroPie distribution, it does not install X and emulation station can be turned off easily.
+
+* Requirements:
+
+iCloud Access module https://github.com/picklepete/pyicloud
+
+fbi needs to be installed.
+
+* Usage
+
+export USERNAME='YOURAPPLEID'
+python iCloudPhotoViewer
+
+At the moment the "fbi" app is used to show the image, it will be startet with sudo to get access to /dev/fb0, normaly this works on the pi without password prompt. If not you can change the access right to the /dev/fb0 device.
---
+++ iCloudPhotoViewer.py
@@ -0,0 +1,63 @@
+from pyicloud import PyiCloudService
+from sys import exit
+from os import environ, system
+from random import choice
+from time import sleep
+from getpass import getpass
+
+username = environ['USERNAME']
+# password = environ['PASSWORD']
+password = getpass("Enter iCloud Password for %s: "%username)
+
+
+api = PyiCloudService(username, password)
+
+if api.requires_2sa:
+    print "Two-step authentication required. Your trusted devices are:"
+    devices = api.trusted_devices
+    #print devices
+    for i, device in enumerate(devices):
+        print "  %s: %s" % (i, device.get('deviceName', "SMS to %s" % device.get('phoneNumber')))
+    device = devices[0]
+    print device
+    if not api.send_verification_code(device):
+        print "Failed to send verification code"
+        exit(1)
+    code = raw_input("Enter Verification Code: ")
+    print code
+    if not api.validate_verification_code(device, code):
+        print "Failed to verify verification code"
+        exit(1)
+
+print "Auth OK !"
+# print api.iphone.play_sound()
+# print api.iphone.location()
+
+for album in api.photos.albums:
+    print "Album", album.title()
+
+photos = api.photos.albums['2017 Segeln']
+print type(photos)
+photolist = []
+for photo in photos:
+    photolist.append(photo)
+#for photo in photolist:
+#    print photo, photo.filename
+
+print "Fotos in list:", len(photolist)
+
+filename = "photo.jpg"
+while(1):
+    photo = choice(photolist)
+    print photo.filename, photo.size, photo.dimensions
+    if photo.dimensions[0] * photo.dimensions[1] < 15000000:
+        download = photo.download('medium')
+        with open(filename, 'wb') as opened_file:
+            opened_file.write(download.raw.read())
+
+        system("sudo fbi -T 2 -d /dev/fb0 -a --noverbose %s"%(filename))
+
+        sleep(30)
+    else:
+        print "skipping large photo"
+
